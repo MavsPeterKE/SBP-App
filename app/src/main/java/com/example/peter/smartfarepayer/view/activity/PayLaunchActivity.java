9f1673717347f8e493a3d6fa3f5efa4b3fedb213
peterOnyango <mavspeter@gmail.com>
2018-04-10 11:58:08 +0300

    Report Complain

--- app/src/main/java/com/example/peter/smartfarepayer/view/activity/PayLaunchActivity.java
+++ app/src/main/java/com/example/peter/smartfarepayer/view/activity/PayLaunchActivity.java
@@ -75,12 +75,12 @@ public class PayLaunchActivity extends AppCompatActivity
             // Handle the camera action
             startActivity(new Intent(this, MainActivity.class));
         } else if (id == R.id.nav_gallery) {
-            startActivity(new Intent(this, ComplainActivity.class));
+
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
+            startActivity(new Intent(this, ComplainActivity.class));
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
